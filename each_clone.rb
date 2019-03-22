52d971ae9033ce09efed2c156b42fd95516e4fd7
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-03-22 14:15:07 +0300

    added rubocop -a

--- each_clone.rb
+++ each_clone.rb
@@ -1,14 +1,13 @@
 # This is our clone of the each method
 module Enumerable
-    def each_clone
-      i = 0 if block_given?
-      begin
-        yield(self[i])
-        i += 1
-      end while i < length 
-      self
-    end
+  def each_clone
+    i = 0 if block_given?
+    begin
+      yield(self[i])
+      i += 1
+    end while i < length
+    self
   end
-  
-  [1, 2, 2, 2, 3, 4, 6].each_clone { |var| print "#{var} " }
-  
+  end
+
+[1, 2, 2, 2, 3, 4, 6].each_clone { |var| print "#{var} " }
