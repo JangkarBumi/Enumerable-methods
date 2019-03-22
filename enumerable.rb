a798f49a14355781e3d5a8cc516c488b735b912c
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-03-22 14:48:42 +0300

    added my_each_with_index method

--- enumerable.rb
+++ enumerable.rb
@@ -8,6 +8,16 @@ module Enumerable
     end
     self
   end
+
+  def my_each_with_index
+    i = 0 if block_given?
+    while i < length
+      yield(self[i], i)
+      i += 1
+    end
+    self
+  end
 end
 
 [1, 2, 2, 2, 3, 4, 6].my_each { |var| print var * 2 }
+[1, 2, 2, 2, 3, 4, 6].my_each_with_index { |var, index| print "#{index}:#{var} " }
