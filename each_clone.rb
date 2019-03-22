2850dc35260f1b410721eb8bfde2a33b949c7b94
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-03-22 14:09:30 +0300

    intial commit

---
+++ each_clone.rb
@@ -0,0 +1,14 @@
+# This is our clone of the each method
+module Enumerable
+    def each_clone
+      i = 0 if block_given?
+      begin
+        yield(self[i])
+        i += 1
+      end while i < length 
+      self
+    end
+  end
+  
+  [1, 2, 2, 2, 3, 4, 6].each_clone { |var| print "#{var} " }
+  
